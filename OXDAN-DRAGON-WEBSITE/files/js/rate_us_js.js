f5c87b373ed7d85fb7fcf15a4421cc8dcff4759c
Mester re <bogdan2010gladiy@gmail.com>
2023-09-14 18:20:44 -0400

    small change

--- OXDAN-DRAGON-WEBSITE/files/js/rate_us_js.js
+++ OXDAN-DRAGON-WEBSITE/files/js/rate_us_js.js
@@ -1,41 +1,30 @@
-//localStorage.clear();
-
 const stars = document.querySelectorAll('.star');
 const progressContainer = document.querySelector('.progress-container');
 const progressBar = document.querySelector('.progress-bar');
 const progressText = document.querySelector('#progress-text');
 const thanksMessage = document.querySelector('#thanks-message');
-
-let userHasRated = false;
-let totalRatings = 0;
-let userRating = 0;
-
-if (localStorage.getItem('userRating')) {
-    userRating = parseInt(localStorage.getItem('userRating'));
-    userHasRated = true;
-    stars[userRating - 1].classList.add('active');
-    updateProgress();
-}
+let rating = 0;
 
 stars.forEach((star) => {
-    star.addEventListener('click', () => {
-        if (!userHasRated) {
-            userRating = parseInt(star.getAttribute('data-rating'));
-            userHasRated = true;
-            localStorage.setItem('userRating', userRating);
-            updateProgress();
-            showThanksMessage();
-        }
-    });
+  star.addEventListener('click', () => {
+    rating = parseInt(star.getAttribute('data-rating'));
+    updateStars();
+  });
 });
 
-function updateProgress() {
-    if (userHasRated) {
-        totalRatings += userRating;
-        const progressPercentage = (totalRatings / 5) * 100;
-        progressBar.style.width = `${progressPercentage}%`;
-        progressText.textContent = `Progress: ${progressPercentage.toFixed(2)}%`;
+function updateStars() {
+  stars.forEach((star, index) => {
+    if (index < rating) {
+      star.classList.add('active');
+    } else {
+      star.classList.remove('active');
     }
+  });
+
+  const progressWidth = (rating / 5) * 100;
+  progressBar.style.width = progressWidth + "%";
+  progressText.textContent = `Overal: ${progressWidth.toFixed(2)}%`;
+  showThanksMessage();
 }
 
 function showThanksMessage() {
